c687d7b6aa72c51b0fcebbe7657feac70925b9bf
Mia Dinh <miadinh38@gmail.com>
2023-08-20 22:11:24 -0400

    Implement promises and delete comment-out code

--- index2.js
+++ index2.js
@@ -1,4 +1,4 @@
-const { nextISSTimesForMyLocation } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss_promised');
 
 const printPassTimes = function(passTimes) {
   for (const pass of passTimes) {
@@ -9,10 +9,10 @@ const printPassTimes = function(passTimes) {
   }
 };
 
-nextISSTimesForMyLocation((error, passTimes) => {
-  if (error) {
-    return console.log("It didn't work!", error);
-  }
-  // success, print out the deets!
-  printPassTimes(passTimes);
-});
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
